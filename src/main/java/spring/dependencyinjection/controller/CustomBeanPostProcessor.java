23f0ca3cc5a68eadcdbd650d39c00407db38d585
CristianSw <cristian.sw@protonmail.com>
2020-03-20 14:50:30 +0200

    Bean life cycle added

---
+++ src/main/java/spring/dependencyinjection/controller/CustomBeanPostProcessor.java
@@ -0,0 +1,23 @@
+package spring.dependencyinjection.controller;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import spring.dependencyinjection.services.BeanLifeCycle;
+
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+       if (bean instanceof BeanLifeCycle){
+           ((BeanLifeCycle) bean).beforeInit();
+       }
+       return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof BeanLifeCycle){
+            ((BeanLifeCycle) bean).afterInit();
+        }
+        return bean;
+    }
+}
